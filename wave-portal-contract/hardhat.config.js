fe8d3604b84658a597afd71d9b160aeed2bad41c
sonolibero.io <ciao@sonolibero.io>
2022-11-11 23:29:11 +0100

    final contract

--- wave-portal-contract/hardhat.config.js
+++ wave-portal-contract/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.STAGING_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
